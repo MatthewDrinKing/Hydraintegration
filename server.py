43a92df4c050ab8d86e4f7ca49df9c7b05f7fa08
freddie <matthew@drinking.co.uk>
2024-07-03 12:07:15 +0100

    adding the curl commands for future reference

--- server.py
+++ server.py
@@ -28,3 +28,9 @@ print("Execute Ticket Command (JS Disabled) Response:", response.status_code, re
 # Execute ticket command (response JS enabled)
 response = requests.get(f"http://{hydra_ip}/_io?cmd=5&js=1")
 print("Execute Ticket Command (JS Enabled) Response:", response.status_code, response.text)
+
+
+#curl -X GET "http://192.168.1.136/_io?cmd=0"
+#curl -X GET "http://192.168.1.136/_io?cmd=4&js=1&pkt=v"
+#curl -X POST "http://192.168.1.136/_fileio?cmd=3" -H "Content-Type: text/plain" --data-binary $'3/S/Articolo di test//1/1/1//\n7/1/1/8/ Cliente 123 #/\n5/1/0////'
+#curl -X GET "http://192.168.1.136/_io?cmd=5&js=1"
